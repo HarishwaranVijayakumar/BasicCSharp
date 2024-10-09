598e574f2c0bb15f341835dbaf4e8069b935084e
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2024-10-09 15:42:30 +0530

    Adding Basic CSharp files

---
+++ ArrayAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace ArrayAssignment;
+class Program
+{
+    public static void Main(string []args)
+    {
+        // create array of size 5 and add names
+        string [] names=new string[5]{"Hari","Ganesh","Venkat","Suresh","Venkat"};
+        // print names using for
+        for(int i=0;i<names.Length;i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+        // get a name from the user to search
+        Console.WriteLine("Enter a name to search");
+        string name=Console.ReadLine();
+        // search using for loop and print index
+        int flag=0;
+        for(int i=0;i<names.Length;i++)
+        {
+            if(names[i]==name)
+            {
+                Console.WriteLine($"The name is present in the array in the index of {i}");
+                flag=1;
+                // break;
+            }
+        }
+        if(flag==0)
+        {
+            Console.WriteLine($"The name is not present in the array");
+        }
+        // search using foreach
+        int flag1=0;
+        foreach(string i in names)
+        {
+            if(i==name)
+            {
+                Console.WriteLine("The name is present in the array");
+                flag1=1;
+            }
+        }
+        if(flag1==0)
+        {
+            Console.WriteLine("The name is not present in the array");
+        }
+        
+    }
+}
---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace DoWhileLoop;
+class Program
+{
+    public static void Main(string []args)
+    {
+        // get the number and check odd or even
+        int number;
+        string option="yes";
+        do{
+            if(option=="yes")
+            {
+                Console.WriteLine("Enter the number");
+                number=Convert.ToInt32(Console.ReadLine());
+                if(number%2==0)
+                { 
+                    Console.WriteLine("The number is even");
+                }
+                else
+                {
+                    Console.WriteLine("The number is odd");
+                }
+                Console.WriteLine("Do you want the process to repeat");
+                option=Console.ReadLine();
+            }
+            else if(option!="yes" || option!="no")
+            {
+                Console.WriteLine("The provided input is wrong and enter the correct input.");
+                Console.WriteLine("Do you want the process to repeat");
+                option=Console.ReadLine();
+            }
+            // ask the user to repeat the process 
+            
+        }while (option!="no");
+    }
+}
